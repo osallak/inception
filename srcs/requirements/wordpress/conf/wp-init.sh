c0c4a9ddee3c9d37f727a7d3827f7d15ac8dee4a
Oussama Sallak <osallak@student.1337.ma>
2023-06-04 10:17:02 -0400

    mandatory part finished without docker-compose and makefile

--- srcs/requirements/wordpress/conf/wp-init.sh
+++ srcs/requirements/wordpress/conf/wp-init.sh
@@ -1,7 +1,4 @@
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-
-chown -R www-data:www-data /var/www/
-
 wp core download --path=/var/www/ --allow-root
 
 cat /var/www/wp-config-sample.php >  /var/www/wp-config.php
@@ -12,6 +9,8 @@ wp config set DB_PASSWORD $USER_PASS --path=/var/www/ --allow-root
 wp config set DB_HOST $DB_HOST --path=/var/www/ --allow-root
 
 
-wp core install --path=/var/www/ --url=$DOMAIN --title=$DOMAIN --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
+wp core install --path=/var/www/ --url=$DOMAIN --title=inception --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
 
-wp user create --path=/var/www/ $USER_NAME $USER_EMAIL --user_pass=$USER_PASS --role=author --allow-root
+wp user create --path=/var/www/ $USER_NAME $USER_EMAIL --user_pass=$USER_PASS --role=$USER_ROLE --allow-root
+
+php-fpm7.4 -F
